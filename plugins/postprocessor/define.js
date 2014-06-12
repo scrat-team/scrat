620720bf8169efd0eebb461ae73e648edcff9fb7
Hinc <liuyang@ucweb.com>
2014-06-12 15:49:19 +0800

    增加对 component.json exports 支持

--- plugins/postprocessor/define.js
+++ plugins/postprocessor/define.js
@@ -1,6 +1,17 @@
 module.exports = function(content, file){
     if(file.isMod){
-        content = 'define(\'' + file.getId() + '\', function(require, exports, module){' + content + '\n\n});';
+        var componentJson = file.dirname + '/component.json';
+        var exports = '';
+        if(fis.util.isFile(componentJson)){
+            var json = require(componentJson);
+            if(json.exports){
+                var main = fis.util(file.dirname, json.main || 'index.js');
+                if(main === file.realpath){
+                    exports = ';module.exports = ' + json.exports;
+                }
+            }
+        }
+        content = 'define(\'' + file.getId() + '\', function(require, exports, module){' + content + exports + '\n\n});';
     }
     return content;
 };
