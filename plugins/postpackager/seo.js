062961abfaadfbc910d1da9abeb600c564736be2
fouber <fouber.NET@gmail.com>
2015-06-28 22:11:18 +0800

    [UPD] SEO模式支持构建版本校验

--- plugins/postpackager/seo.js
+++ plugins/postpackager/seo.js
@@ -1,6 +1,7 @@
-module.exports = function(ret, conf, settings, opt){
-  var framework = fis.config.get('framework');
-  framework.res = ret.map.res;
-  ret.map = framework;
-  ret.map.combo = !!opt.pack;
+module.exports = function (ret, conf, settings, opt) {
+    var framework = fis.config.get('framework');
+    framework.res = ret.map.res;
+    framework.hash = fis.util.md5(Date.now() + '-' + Math.random());
+    ret.map = framework;
+    ret.map.combo = !!opt.pack;
 };
