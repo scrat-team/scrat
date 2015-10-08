ee3045642b2d975f5cbb0a753124d789644ee8dd
TZ <atian25@qq.com>
2015-10-08 16:03:25 +0800

    support suger method: scrat publish

--- index.js
+++ index.js
@@ -152,6 +152,27 @@ fis.seo = function(name){
   fis.config.set('modules.postpackager', require('./plugins/postpackager/seo.js'));
 };
 
+//发布指令, 用法 scrat publish, 相当于 scrat release -opmcuD
+fis.require._cache['command-publish'] = {
+    name: 'publish',
+    desc: 'short cmd for release',
+    register: function(commander){
+        commander
+            .option('-d, --dest <names>', 'release output destination', String, 'zip')
+            .option('-f, --file <name>', 'release output fileName', String)
+            .action(function() {
+                var argv = process.argv;
+                var options = arguments[arguments.length - 1];
+                var newArgv = argv.slice(0, 2).concat(['release', '-opmcuDd', options.dest]);
+                if(options.file){
+                    fis.config.set('settings.deploy.compress.zip.file', options.file);
+                }
+                fis.log.notice('publish by: `scrat ' + newArgv.slice(2).join(' ') + '`');
+                fis.cli.run(newArgv);
+            });
+  }
+};
+
 //alias
 Object.defineProperty(global, 'scrat', {
     enumerable : true,
