1cf273da7939de5d51b64e3a4495bd8866fdfb84
Hinc Liu <hincubus@gmail.com>
2015-10-16 16:37:31 +0800

    移除老 Lego 模式代码

--- index.js
+++ index.js
@@ -32,58 +32,6 @@ fis.olpm.PACK_TYPE_INLINE   = 1;
 fis.olpm.PACK_TYPE_EXTERNAL = 2;
 //fis.olpm.PACK_TYPE_COMBO    = 3;
 
-fis.lego = function (info) {
-    fis.cache.clean('compile');
-
-    if (typeof info === 'string') info = {code : info};
-    info.hash = fis.util.md5(Date.now() + '-' + Math.random());
-    fis.config.set('lego', info);
-    if (!info.code) {
-        fis.log.error('missing project code, use `fis.config.set("lego.code", value);` in fis-conf.js');
-        return process.exit(1);
-    }
-    if (!info.version) info.version = '_';
-    var domain = 'http://image.uc.cn';
-    if (info.hasOwnProperty('domain') && info.domain) {
-        domain = info.domain.replace(/\/$/, '');
-    }
-    fis.config.set('roadmap.domain', domain);
-    fis.config.set('roadmap.path', require('./configs/lego'));
-    fis.config.set('modules.preprocessor.css', [require('./plugins/preprocessor/lego').CSS]);
-    fis.config.set('modules.preprocessor.js', [require('./plugins/preprocessor/lego').JS]);
-    fis.config.set('modules.preprocessor.html', [require('./plugins/preprocessor/lego').HTML]);
-    fis.config.set('modules.postprocessor.css', []);
-    fis.config.set('modules.postprocessor.js', [require('./plugins/postprocessor/lego').JS]);
-    fis.config.set('modules.postprocessor.html', []);
-    fis.config.set('modules.prepackager', []);
-    fis.config.set('modules.postpackager', [require('./plugins/postpackager/lego')]);
-    if (info.nightcss) fis.config.get('modules.preprocessor.css').push('nightcss');
-
-    fis.config.set('settings.lint.jshint', {
-        // Enforcing options
-        camelcase: true,
-        eqeqeq: true,
-        forin: true,
-        freeze: true,
-        globals: ['__inline', '__uri', 'lego'],
-        immed: true,
-        indent: 2,
-        newcap: true,
-        noarg: true,
-        noempty: true,
-        nonew: true,
-        quotmark: 'single',
-        undef: true,
-        unused: true,
-        strict: true,
-
-        // Environments
-        browser: true,
-        jquery: true,
-        node: true
-    });
-};
-
 fis.seo = function(name){
   if(typeof name === 'object'){
     fis.config.merge(name);
